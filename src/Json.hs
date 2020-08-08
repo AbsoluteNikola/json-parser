733d2d0728f9803fa6ae17b10d9823c5649a77fd
Nikolay Rulev <nikolayr2001@gmail.com>
2020-08-08 19:07:43 +0300

    add bool and null parsers

--- src/Json.hs
+++ src/Json.hs
@@ -1,8 +1,32 @@
 module Json where
 
+import Control.Applicative ((<|>))
+
+import Parser (Parser, runParser)
+import Primitives (charP, stringP)
+
 data Json = JsonNull
   | JsonBool Bool
   | JsonString String
   | JsonNumber Double
   | JsonArray [Json]
   | JsonObject [(String, Json)]
+  
+jsonNullP :: Parser Json
+jsonNullP = JsonNull <$ stringP "null"
+
+jsonBoolP :: Parser Json
+jsonBoolP = helper <$> (stringP "true" <|> stringP "false")
+  where
+    helper "true" = JsonBool True
+    helper _ = JsonBool False
+    -- helper will never be called without "true" or "false"
+    
+jsonP :: Parser Json
+jsonP = jsonNullP <|> jsonBoolP
+
+parseJson :: String -> Maybe Json
+parseJson input =
+  case runParser jsonP input of
+    Nothing -> Nothing
+    Just (_, result) -> Just result
